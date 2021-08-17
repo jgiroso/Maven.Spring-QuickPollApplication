f69d8b13bdfd7d2619867342c4f757f4de774b17
jgiroso <jrgiroso@gmail.com>
2021-08-17 11:46:09 -0400

    part 3 complete. voteController created

--- src/main/java/io/zipcoder/tc_spring_poll_application/controller/VoteController.java
+++ src/main/java/io/zipcoder/tc_spring_poll_application/controller/VoteController.java
@@ -1,9 +1,9 @@
 package io.zipcoder.tc_spring_poll_application.controller;
 
-import io.zipcoder.tc_spring_poll_application.domain.Poll;
 import io.zipcoder.tc_spring_poll_application.domain.Vote;
 import io.zipcoder.tc_spring_poll_application.repositories.VoteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -21,39 +21,24 @@ public class VoteController {
         this.voteRepository = voteRepository;
     }
 
-    @GetMapping("/votes")
-    public ResponseEntity<Iterable<Vote>> getAllVotes() {
-        Iterable<Vote> allVotes = voteRepository.findAll();
-        return new ResponseEntity<>(allVotes, HttpStatus.OK);
-    }
-
-    @PostMapping("/votes")
-    public ResponseEntity<?> createVote(@RequestBody Vote vote) {
+    @PostMapping("/polls/{pollId}/votes")
+    public ResponseEntity<?> createVote(@PathVariable Long pollId, @RequestBody Vote
+            vote) {
         vote = voteRepository.save(vote);
-        URI newPollUri = ServletUriComponentsBuilder
-                .fromCurrentRequest()
-                .path("/{id}")
-                .buildAndExpand(vote.getId())
-                .toUri();
-        return new ResponseEntity<>(newPollUri, HttpStatus.CREATED);
+        // Set the headers for the newly created resource
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.setLocation(ServletUriComponentsBuilder.
+                fromCurrentRequest().path("/{id}").buildAndExpand(vote.getId()).toUri());
+        return new ResponseEntity<>(null, responseHeaders, HttpStatus.CREATED);
     }
 
-    @GetMapping("/votes/{voteId}")
-    public ResponseEntity<?> getVote(@PathVariable Long voteId) {
-        Vote v = voteRepository.findOne(voteId);
-        return new ResponseEntity<> (v, HttpStatus.OK);
+    @GetMapping("/polls/votes")
+    public Iterable<Vote> getAllVotes() {
+        return voteRepository.findAll();
     }
 
-    @PutMapping("/votes/{voteId}")
-    public ResponseEntity<?> updateVote(@RequestBody Vote vote, @PathVariable Long voteId) {
-        // Save the entity
-        Vote v = voteRepository.save(vote);
-        return new ResponseEntity<>(HttpStatus.OK);
-    }
-
-    @DeleteMapping("/votes/{voteId}")
-    public ResponseEntity<?> deleteVote(@PathVariable Long voteId) {
-        voteRepository.delete(voteId);
-        return new ResponseEntity<>(HttpStatus.OK);
+    @GetMapping("polls/{pollId}/votes")
+    public Iterable<Vote> getVote(@PathVariable Long pollId) {
+        return voteRepository.findVotesByPoll(pollId);
     }
 }
